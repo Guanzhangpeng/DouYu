6686fe713fe02f442b207343f7bf6c074443a0a9
gzp <zswangzp@163.com>
2017-10-09 13:18:40 +0800

    项目初始化

---
+++ DouYu/DouYu/Classes/Main/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  DouYu
+//
+//  Created by 管章鹏 on 2017/10/9.
+//  Copyright © 2017年 管章鹏. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+}
+extension MainViewController
+{
+    fileprivate func setupUI()
+    {
+        setupTabbar(name: "Home")
+        setupTabbar(name: "Live")
+        setupTabbar(name: "Follow")
+        setupTabbar(name: "Profile")
+    }
+    
+    fileprivate func setupTabbar(name : String)
+    {
+        let vc = UIStoryboard(name:name, bundle: nil).instantiateInitialViewController()!
+        addChildViewController(vc)
+    }
+}
